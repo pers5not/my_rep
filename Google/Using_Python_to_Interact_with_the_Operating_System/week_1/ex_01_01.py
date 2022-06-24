86a25d5fcf8bcd282ee72f7786bebc775ac40a7f
pers5not <pers5not@gmail.com>
2022-06-24 11:37:25 +0300

    commit_0_0_1

--- Google/Using_Python_to_Interact_with_the_Operating_System/week_1/ex_01_01.py
+++ Google/Using_Python_to_Interact_with_the_Operating_System/week_1/ex_01_01.py
@@ -1,1 +1,13 @@
-print('Hello GitHub')
+from PIL import Image
+import random
+import pandas
+image = Image.open(
+    "Using_Python_to_Interact_with_the_Operating_System/week_1/av_8.png")
+print(image.size)
+print(image.format)
+visitors = [random.randint(1111, 5555) for i in range(5)]
+errors = [random.randint(11, 100) for i in range(5)]
+df = pandas.DataFrame(
+    {'visitors': visitors, 'errors': errors}, index=['Mon', 'Tues', 'Wed', 'Thu', 'Fri'])
+
+print(df)
