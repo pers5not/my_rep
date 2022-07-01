ec8f465fd47135743283c8f772d7ac8f3ff3bc94
pers5not <pers5not@gmail.com>
2022-07-01 12:42:22 +0300

    commit_05_01_01

--- Google/Using_Python_to_Interact_with_the_Operating_System/file_generator.py
+++ Google/Using_Python_to_Interact_with_the_Operating_System/file_generator.py
@@ -1,3 +1,3 @@
 for i in range(1, 20):
     fhand = open(
-        f'/home/johndoe/MY_education/Coursera/my_rep/Google/Using_Python_to_Interact_with_the_Operating_System/week_4/ex_04_{i}.py', 'w')
+        f'/home/johndoe/MY_education/Coursera/my_rep/Google/Using_Python_to_Interact_with_the_Operating_System/week_5/ex_05_{i}.py', 'w')
