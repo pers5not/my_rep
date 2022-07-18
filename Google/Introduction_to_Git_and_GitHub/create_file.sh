db8272a87d28f05a6849394ba22e1edf3e2e281f
pers5not <pers5not@gmail.com>
2022-07-18 12:36:40 +0300

    script to create files and give them  to execute

--- Google/Introduction_to_Git_and_GitHub/create_file.sh
+++ Google/Introduction_to_Git_and_GitHub/create_file.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
 
 comand=$1
-echo '#!/usr/bin/env python3' > $comand| chmod +x $comand | code $comand
+echo '#!/usr/bin/env python3.' > $comand| chmod +x $comand | code $comand
+#Скрипт
