5365a156d48302e54cb13b4bfc27eb99150c5b8b
pers5not <pers5not@gmail.com>
2022-07-19 20:06:55 +0300

    rename min_absolute to min_gb

--- Google/Introduction_to_Git_and_GitHub/week_3/all_checks.py
+++ Google/Introduction_to_Git_and_GitHub/week_3/all_checks.py
@@ -13,14 +13,14 @@ def check_reboot():
     return os.path.exists("/run/reboot-required")
 
 
-def check_disk_full(disk, min_absolute, min_percent):
+def check_disk_full(disk, min_gb, min_percent):
     """Return True if there isn't enougth disk space, False otherwise"""
     du = shutil.disk_usage(disk)
     # Calculate the percentage of free space
     percent_free = 100 * du.free / du.total
     # Calculate how many free gygabates
     gygabates_free = du.free / 2 ** 30
-    if percent_free < min_percent or gygabates_free < min_absolute:
+    if percent_free < min_percent or gygabates_free < min_gb:
         return True
     return False
 
@@ -29,11 +29,12 @@ def main():
     if check_reboot():
         print("Pending reboot")
         sys.exit(1)
-    if check_disk_full('/', 2, 10):
+    if check_disk_full(disk='/', min_gb=2, min_percent=10):
         print("Disk Full.")
         sys.exit(1)
-        
+
     print("Everything ok.")
     sys.exit(0)
+
 
 main()
